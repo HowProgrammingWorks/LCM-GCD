aad8154e9ab6814b97150f4ea920c5a9c4c51ed4
helgarden <slutska.k@gmail.com>
2018-11-26 11:07:10 +0200

    Renamed functions

--- JavaScript/1-euclidean-loop.js
+++ JavaScript/1-euclidean-loop.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const algorithm = (num1, num2) => {
+const euclideanAlgorithm = (num1, num2) => {
   while (num2) {
     num2 = num1 % (num1 = num2);
   }
   return (Math.abs(num1));
 };
 
-console.log(algorithm(10, 18));
-console.log(algorithm(17, 34));
-console.log(algorithm(-15, 30));
-console.log(algorithm(7, 11));
-console.log(algorithm(-10, -15));
-console.log(algorithm(0, 4));
-console.log(algorithm(-7, 0));
+console.log(euclideanAlgorithm(10, 18));
+console.log(euclideanAlgorithm(17, 34));
+console.log(euclideanAlgorithm(-15, 30));
+console.log(euclideanAlgorithm(7, 11));
+console.log(euclideanAlgorithm(-10, -15));
+console.log(euclideanAlgorithm(0, 4));
+console.log(euclideanAlgorithm(-7, 0));
--- JavaScript/2-euclidean-recursion.js
+++ JavaScript/2-euclidean-recursion.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const euclideanRecursion = (num1, num2) => {
-  if (!num2) {
+const euclideanAlgorithm = (num1, num2) => {
+  if (!num2)
     return (Math.abs(num1));
-  } else {
-    return euclideanRecursion(num2, num1 % num2);
-  }
+  else
+    return euclideanAlgorithm(num2, num1 % num2);
 };
 
-console.log(euclideanRecursion(10, 18));
-console.log(euclideanRecursion(17, 34));
-console.log(euclideanRecursion(-15, 30));
-console.log(euclideanRecursion(7, 11));
-console.log(euclideanRecursion(-10, -15));
-console.log(euclideanRecursion(0, 4));
-console.log(euclideanRecursion(-7, 0));
+console.log(euclideanAlgorithm(10, 18));
+console.log(euclideanAlgorithm(17, 34));
+console.log(euclideanAlgorithm(-15, 30));
+console.log(euclideanAlgorithm(7, 11));
+console.log(euclideanAlgorithm(-10, -15));
+console.log(euclideanAlgorithm(0, 4));
+console.log(euclideanAlgorithm(-7, 0));
