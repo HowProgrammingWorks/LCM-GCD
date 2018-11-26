d56d7d56a2e69cf7b656704408538321d0ebe7a8
helgarden <slutska.k@gmail.com>
2018-11-26 11:03:13 +0200

    Added recursive variant

--- JavaScript/2-euclidean-recursion.js
+++ JavaScript/2-euclidean-recursion.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const euclideanRecursion = (num1, num2) => {
+  if (!num2) {
+    return (Math.abs(num1));
+  } else {
+    return euclideanRecursion(num2, num1 % num2);
+  }
+};
+
+console.log(euclideanRecursion(10, 18));
+console.log(euclideanRecursion(17, 34));
+console.log(euclideanRecursion(-15, 30));
+console.log(euclideanRecursion(7, 11));
+console.log(euclideanRecursion(-10, -15));
+console.log(euclideanRecursion(0, 4));
+console.log(euclideanRecursion(-7, 0));
